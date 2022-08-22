e68be3a57a6b9507b1c44890f254e7256762bb5d
Arafat <arafatamrullaev021@gmail.com>
2022-08-23 00:47:04 +0600

    is_public

--- config/settings.py
+++ config/settings.py
@@ -102,14 +102,14 @@ DATABASES = {
     }
 }
 
-# import dj_database_url
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql'
-#     }
-# } 
-# db = dj_database_url.config(conn_max_age=600)
-# DATABASES['default'].update(db)
+import dj_database_url
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql'
+    }
+} 
+db = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db)
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
