02d538e00c9474bf663de2ce27647c139b5d4258
Arafat <arafatamrullaev021@gmail.com>
2022-08-22 21:44:26 +0600

    is_public

--- music/permissions.py
+++ music/permissions.py
@@ -9,9 +9,12 @@ class IsAdminOrReadOnly(BasePermission):
             request.user.is_staff
 
 class IsAuthor(BasePermission):
-    def has_permission(self, request, view, obj):
-        return bool(request.user and request.user == obj.user)
+    def has_object_permission(self, request, view, obj):
+        return bool(request.user.is_authenticated and request.user == obj.user)
 
 class IsAuthorOrReadOnly(BasePermission):
-    def has_object_permission(self, request, view, obj):
-        return request.user.is_authenticated and request.user == obj.user
+    def has_objects_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        else:
+            request.user.is_authenticated and request.user == obj.user
