0b8261fe4a7f04d8ce2e2d2ea395cf67d508e841
Arafat <arafatamrullaev021@gmail.com>
2022-08-22 21:15:33 +0600

    myplaylist

--- music/serializers.py
+++ music/serializers.py
@@ -48,7 +48,7 @@ class AlbumSerializer(serializers.ModelSerializer):
 
 class MyPlaylistSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Comment
+        model = MyPlaylist
         exclude = ['user']
 
     def create(self, validated_data):
