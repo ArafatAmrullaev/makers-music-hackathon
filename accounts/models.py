f47ea649d2b5f72fb034d6b33a660ce493745669
Arafat <arafatamrullaev021@gmail.com>
2022-08-27 19:48:48 +0600

    activation

--- accounts/models.py
+++ accounts/models.py
@@ -73,6 +73,6 @@ class User(AbstractUser):
     def send_activation_code(self):
         from django.core.mail import send_mail
         self.generate_activation_code()
-        activation_url = f'http://127.0.0.1:8000/account/activate/{self.activation_code}/'
+        activation_url = f'https://makerskg-music.herokuapp.com/activate/{self.activation_code}/'
         message = f'Activate your account, following this link {activation_url}'
         send_mail("Activate account", message, "spotify@gmail.com", [self.email])
